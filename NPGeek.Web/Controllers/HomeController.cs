e5be1794ef67f47224bae1e2b6420678a0fd4011
Dominic Bartel <DominicBartel@gmail.com>
2018-03-15 16:10:05 -0400

    made functioning dropdown for temperatre choosing, stores selection in session

--- NPGeek.Web/Controllers/HomeController.cs
+++ NPGeek.Web/Controllers/HomeController.cs
@@ -11,47 +11,65 @@ namespace NPGeek.Web.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+
+
         public ActionResult Index()
         {
             ParkIndexModel parks = new ParkIndexModel();
+            parks.Fahrenheit = CheckTemp();
             using (var context = new ParkSystemDatabaseEntities())
             {
                 parks.Parks = context.parks.ToList();
             }
-                return View(parks);
+            return View(parks);
         }
 
         public ActionResult Detail(string parkId)
         {
             ParkDetailModel park = new ParkDetailModel();
+            park.Fahrenheit = CheckTemp();
             using (var context = new ParkSystemDatabaseEntities())
             {
                 park.Park = context.parks.Where(p => p.parkCode == parkId).FirstOrDefault();
                 park.WeatherModels = context.weathers.Where(w => w.parkCode == parkId).ToList();
             }
-                return View(park);
+            return View(park);
         }
 
         public ActionResult Survey()
         {
             SurveyModel survey = new SurveyModel();
+            survey.Fahrenheit = CheckTemp();
             using (var context = new ParkSystemDatabaseEntities())
             {
                 survey.Parks = context.parks.ToList();
             }
-                return View(survey);
+            return View(survey);
         }
 
         public ActionResult SurveyResult()
         {
-
             SurveyResultsModel surveyResult = new SurveyResultsModel();
+            surveyResult.ParkSurveyList = new List<ParkSurveyCount>();
+            surveyResult.Fahrenheit = CheckTemp();
             using (var context = new ParkSystemDatabaseEntities())
             {
-                surveyResult.SurveyResult = context.survey_result.ToList();
-                surveyResult.Parks = context.parks.ToList();
+                foreach(var park in context.survey_result)
+
+                foreach (var survey in context.survey_result)
+                {
+                    
+                    ParkSurveyCount count =  new ParkSurveyCount{
+                        park = survey.park,
+                        numberOfSurveys = survey.park.parkCode.Count()
+                    };
+                       
+                    surveyResult.ParkSurveyList.Add(count);
+                    
+                }
             }
-                return View(surveyResult);
+            surveyResult.ParkSurveyList.OrderBy(model => model.numberOfSurveys);
+            return View(surveyResult);
         }
 
         [HttpPost]
@@ -63,14 +81,35 @@ namespace NPGeek.Web.Controllers
                 context.SaveChanges();
 
             }
-                return RedirectToAction("SurveyResult");
+            return RedirectToAction("SurveyResult");
         }
-   
 
         [HttpPost]
-        public ActionResult Index(string temperature)
+        public ActionResult ChangeTemp(string type)
         {
-            return RedirectToAction("Index");
+            bool Fahrenheit = false;
+            if (type == "f")
+            {
+                Fahrenheit = true;
+            }
+
+            Session["Fahrenheit"] = Fahrenheit;
+            return Redirect(Request.UrlReferrer.ToString());
+        }
+
+        public bool CheckTemp()
+        {
+            bool fahrenheit = true;
+            if(Session["Fahrenheit"] == null)
+            {
+                Session["Fahrenheit"] = true;
+            }
+            else
+            {
+                fahrenheit = Convert.ToBoolean(Session["Fahrenheit"]);
+            }
+
+            return fahrenheit;
         }
     }
 }
