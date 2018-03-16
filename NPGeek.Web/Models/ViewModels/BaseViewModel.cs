8ba2bb1109122659f1ea3b6c9689f407be30d26a
John Kenny <jkennytech@gmail.com>
2018-03-16 15:08:12 -0400

    Dom & John - John still fiddling with CSS

--- NPGeek.Web/Models/ViewModels/BaseViewModel.cs
+++ NPGeek.Web/Models/ViewModels/BaseViewModel.cs
@@ -6,8 +6,7 @@ using System.Web;
 namespace NPGeek.Web.Models.ViewModels
 {
     public class BaseViewModel
-    {
-        public park Park { get; set; }
+    {        
         public bool Fahrenheit { get; set; }       
     }
 }
--- NPGeek.Web/Models/ViewModels/ParkDetailModel.cs
+++ NPGeek.Web/Models/ViewModels/ParkDetailModel.cs
@@ -13,13 +13,13 @@ namespace NPGeek.Web.Models.ViewModels
         {
             get
             {
-                var snow = "Weather is expected to be cold, sad and overall terrible. If you really want to come out please bring snow shoes.";
-                var rain = "Pack rain gear and wear waterproof shows.";
-                var thunderStorm = "Please seek shelter and avoid hiking on exposed ridges.";
-                var sun = "Please make sure to wear sunscreen.";
-                var hot = " It is recommended to bring an extra gallon of water";
-                var difference = " The weather may change, please bring breathable layers";
-                var cold = " It may be well below freezing. Please just go home. Please.. You could get hypothermia. Or frostbite. Or Die.";
+                var snow = "It will be cold and snowy. If you come out, please bring snow shoes.";
+                var rain = "Pack rain gear and wear waterproof shoes.";
+                var thunderStorm = "Seek shelter and avoid hiking on exposed ridges.";
+                var sun = "The sun will be out. Please wear sunscreen.";
+                var hot = " Hot weather could lead to dehydration. Bring an extra gallon of water";
+                var difference = " The weather may change. Please bring breathable layers";
+                var cold = " It may be well below freezing. Hypothermia and frostbite are dangers that you will want to avoid.";
                 string returnString = "";
 
                 if(this.WeatherModels.FirstOrDefault().forecast == "snow")
