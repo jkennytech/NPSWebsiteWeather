96959f9f3a7a556a9fa3abae24381ccea11692fd
Dominic Bartel <DominicBartel@gmail.com>
2018-03-16 16:24:22 -0400

    changed a bit of front end issues

--- NPGeek.Web/Models/ViewModels/ParkDetailModel.cs
+++ NPGeek.Web/Models/ViewModels/ParkDetailModel.cs
@@ -14,13 +14,29 @@ namespace NPGeek.Web.Models.ViewModels
             get
             {
                 var snow = "It will be cold and snowy. If you come out, please bring snow shoes.";
-                var rain = "Pack rain gear and wear waterproof shoes.";
+                var rain = " Pack rain gear and wear waterproof shoes.";
                 var thunderStorm = "Seek shelter and avoid hiking on exposed ridges.";
                 var sun = "The sun will be out. Please wear sunscreen.";
-                var hot = " Hot weather could lead to dehydration. Bring an extra gallon of water";
-                var difference = " The weather may change. Please bring breathable layers";
+                var hot = " Hot weather could lead to dehydration. Bring an extra gallon of water.";
+                var difference = " The weather may change. Please bring breathable layers.";
                 var cold = " It may be well below freezing. Hypothermia and frostbite are dangers that you will want to avoid.";
+                int high = 75;
+                int low = 20;
                 string returnString = "";
+                
+                //We put this if statement here intentionally to have it only show the "difference" string if the difference was in Fahrenheit.
+                if ((this.WeatherModels.FirstOrDefault().high - this.WeatherModels.FirstOrDefault().low) > 20)
+                {
+                    returnString += difference;
+                }
+
+
+                if (Fahrenheit != true)
+                {
+                    high = ((high - 32) * 5 / 9);
+                    low = ((low - 32) * 5 / 9);
+                }
+                
 
                 if(this.WeatherModels.FirstOrDefault().forecast == "snow")
                 {
@@ -38,15 +54,11 @@ namespace NPGeek.Web.Models.ViewModels
                 {
                     returnString += sun;
                 }
-                if (this.WeatherModels.FirstOrDefault().high > 75)
+                if (this.WeatherModels.FirstOrDefault().high > high)
                 {
                     returnString += hot;
                 }
-                if ((this.WeatherModels.FirstOrDefault().high - this.WeatherModels.FirstOrDefault().low) > 20)
-                {
-                    returnString += difference;
-                }
-                if (this.WeatherModels.FirstOrDefault().low < 20)
+                if (this.WeatherModels.FirstOrDefault().low < low)
                 {
                     returnString += cold;
                 }
